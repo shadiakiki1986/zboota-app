0fe38c8e11e6705dd1b83d9ac6ea060ffb9d1eef
Shadi Akiki <shadiakiki1986@gmail.com>
2015-02-12 08:47:39 +0200

    just added echo line for version in build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ jarsigner -verify -verbose -certs ~/zboota-app/platforms/android/ant-build/Cordo
 rm -i ~/zboota-app-release-signed.apk
 $ANDROID_HOME/build-tools/21.1.2/zipalign -v 4 ~/zboota-app/platforms/android/ant-build/CordovaApp-release-unsigned.apk ~/zboota-app-release-signed.apk
 
+
 echo "Completed build app for android"
+echo "Version: "
+$ANDROID_HOME/build-tools/21.1.2/aapt dump badging zboota-app-release-signed.apk |head -n 1
 echo "App is now available in ~/zboota-app-release-signed.apk"
 echo "and you could upload it to Google play at https://play.google.com/apps/publish/"
